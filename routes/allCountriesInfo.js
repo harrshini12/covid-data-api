5bf99884cb5aa97282b8c5e5dcdaa8a294c56042
harrshini12 <harrshini.1995@gmail.com>
2023-09-28 13:30:19 +0200

    updated with new api

--- routes/allCountriesInfo.js
+++ routes/allCountriesInfo.js
@@ -19,11 +19,41 @@ router.get("/", async (req, res) => {
         countryName: record.countriesAndTerritories,
         cases: record.cases,
         deaths: record.deaths,
-        population: record.popData2019,
-        date: record.dateRep,
+        population: record.popData2019 || 0, // Default to 0 if population data is missing
       }));
 
-      res.json(countries);
+      // Create an object to store total cases, deaths, and population for each country
+      const countryTotals = {};
+
+      // Calculate totals for each country
+      countries.forEach((country) => {
+        const {
+          continent,
+          countryName,
+          cases,
+          deaths,
+          population,
+        } = country;
+
+        if (!countryTotals[countryName]) {
+          countryTotals[countryName] = {
+            continent,
+            countryName,
+            cases,
+            deaths,
+            population,
+          };
+        } else {
+          countryTotals[countryName].cases += cases;
+          countryTotals[countryName].deaths += deaths;
+          countryTotals[countryName].population += population;
+        }
+      });
+
+      // Convert the object values to an array
+      const countryTotalsArray = Object.values(countryTotals);
+
+      res.json(countryTotalsArray);
     } else {
       console.error("Failed to fetch data from the URL");
       res.status(500).json({ error: "Internal Server Error" });
